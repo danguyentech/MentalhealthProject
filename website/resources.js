140468db5c75e0da014a33d34c671b95b5550f4e
Naomi-Rose Dutta <Naomi-RoseDutta@Naomi-Roses-MacBook-Air.local>
2020-12-08 01:14:14 -0700

    filter function FINALLY woo

--- website/resources.js
+++ website/resources.js
@@ -1,50 +1,11 @@
-function getInfo(info) {
-    let card = document.getElementsByClassName('navCard');
-    document.getElementById(info).classList.add("d-flex");
-    for (y = 0; y < card.length; y++) {
-      card[y].classList.add("d-none");
-    }
-    document.getElementById('backButton').classList.remove('d-none');
-  }
-  function hideInfo() {
-    let info = document.getElementsByClassName('info');
-    let card = document.getElementsByClassName('navCard');
-    for (x = 0; x < info.length; x++) {
-        info[x].classList.remove('d-flex');
-    }
-    for (i = 0; i < card.length; i++) {
-      card[i].classList.remove('d-none');
-    }
-    document.getElementById('backButton').classList.add("d-none");
-  }
-  function hoverStart(card){
-    document.getElementById(card).style.backgroundColor='#FBA90A';
-  }
-  function hoverEnd(card){
-    document.getElementById(card).style.backgroundColor='#B6A9F0';
-  }
+function filterResources() {
+  let selection = document.getElementById("resourceDropDown");
+  let resources = document.getElementsByClassName("resource");
+  for (element of resources) {
+      if (element.id !== selection.value && selection.value !== "all") {
+          element.classList.add("hidden");
+      } else {
+          element.classList.remove("hidden");
+      }
 
-  function filterResources() {
-      [name: 'Depression'],
-      [name: 'Anxiety'],
-      [name: 'Stress']
-      [name: 'Mindfulness']
-      [name: 'Productivity']
-      [name: 'Substance Use and Addiction']
-      [name: 'Trauma']
-      [name: 'Chronic Pain']
-      [name: 'Chronic Illness']
-      [name: 'Loneliness']
-      [name: 'Eating Disorders']
-      [name: 'Suicide Prevention']
-
-      // Self-Management, Cognitive Modification, Skills Training, Illness Management, Passive Symptom Tracking
-
-  }
-
-  let filterResources = info.filter(info => info.card {
-    return info.
-  }
-
-  console.log(filterResources)
-
+// Self-Management, Cognitive Modification, Skills Training, Illness Management, Passive-Symptom Tracking
